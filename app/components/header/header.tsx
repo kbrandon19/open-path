9c35eeb129902e8291144aa924d9f590897170de
Ken <kbrandon319@gmail.com>
2026-01-10 22:06:21 -0500

    Header class name updated

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -19,17 +19,17 @@ function header() {
         <div className='flex w-auto h-auto flex-row gap-10 justify-between'> 
           
           {/* Mobile Menu */}
-          <div className='flex md:hidden px-4 py-2 w-auto h-auto rounded-full flex flex-row items-center gap-2'>Menu <div className='w-6 h-6 rounded-full bg-[#515151]'></div></div>
+          <div className='flex md:hidden px-4 py-2 w-auto h-auto rounded-full flex-row items-center gap-2'>Menu <div className='w-6 h-6 rounded-full bg-[#515151]'></div></div>
 
           {/* Desktop Menu */}
-            <div className='hidden md:flex w-auto h-auto flex-row gap-4 items-center hidden text-sm'>
+            <div className='hidden md:flex w-auto h-auto flex-row gap-4 items-center  text-sm'>
             <div>Find A Provider</div>
             <div>About</div>
             <div>Contact</div>
             <div>FAQ</div>
             </div>
             
-            <div className=' hidden md:flex px-4 py-2 w-auto h-auto rounded-full bg-[#E0B04E] flex flex-row items-center gap-2 text-sm'>
+            <div className=' hidden md:flex px-4 py-2 w-auto h-auto rounded-full bg-[#E0B04E]  flex-row items-center gap-2 text-sm'>
                 Login
 
                 <div className='w-6 h-6 rounded-full bg-[#515151]'></div>
